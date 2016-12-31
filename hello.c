d9a42de4c8daaa5b8fea05f6fe47c6cd3e31b9bf
Philip Bao <bwwyyy@users.noreply.github.com>
2016-12-30 23:30:20 -0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
+// Comment Comment
+void dummy() {
+    return; // in-line comment
+}
 
-int fibonacci(int i) {
-    if (i <= 1) {
+int fibonacci(int i){
+    if (i <= 1) 
+    {
         return 1;
     }
+    if (i == 2) printf("i == 2");
+    if (i == 3) dummy();
     return fibonacci(i-1) + fibonacci(i-2);
 }
 
-int main()
-{
+int main(){
     int i;
     i = 0;
-    while (i <= 10) {
+    int limit = 10;
+    while (i < limit) {
         printf("fibonacci(%2d) = %d\n", i, fibonacci(i));
         i = i + 1;
     }
